41d10e3c3b3fcab8bc669152883b250bbb26494e
addis <betsega23@gmail.com>
2022-09-07 20:48:45 +0300

    Add event  to buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,33 +1,65 @@
 import React from 'react';
 import './styles.css';
+import calculate from '../logic/calculate';
+
+const btns = [
+  { value: 'AC', className: 'item' },
+  { value: '+/-', className: 'item' },
+  { value: '%', className: 'item' },
+  { value: '÷', className: 'item2' },
+  { value: '7', className: 'item' },
+  { value: '8', className: 'item' },
+  { value: '9', className: 'item' },
+  { value: 'x', className: 'item2' },
+  { value: '4', className: 'item' },
+  { value: '5', className: 'item' },
+  { value: '6', className: 'item' },
+  { value: '-', className: 'item2' },
+  { value: '1', className: 'item' },
+  { value: '2', className: 'item' },
+  { value: '3', className: 'item' },
+  { value: '+', className: 'item2' },
+  { value: '0', className: 'item18' },
+  { value: '.', className: 'item' },
+  { value: '=', className: 'item2' },
+];
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onClickHandler = (event) => {
+    this.setState((prevState) => calculate(prevState, event.target.value));
+  };
+
+  formatOutput = () => {
+    const { total, next, operation } = this.state;
+    const display = (total || '') + (operation || '') + (next || '');
+
+    return display || '0';
+  };
+
   render() {
     return (
       <div className="grid-container">
         <div className="item1">
-          <p className="result">0</p>
+          <p className="result">{this.formatOutput()}</p>
         </div>
-        <div className="item">AC</div>
-        <div className="item">+/-</div>
-        <div className="item">%</div>
-        <div className="item2">÷</div>
-        <div className="item">7</div>
-        <div className="item">8</div>
-        <div className="item">9</div>
-        <div className="item2">X</div>
-        <div className="item">4</div>
-        <div className="item">5</div>
-        <div className="item">6</div>
-        <div className="item2">-</div>
-        <div className="item">1</div>
-        <div className="item">2</div>
-        <div className="item">3</div>
-        <div className="item2">+</div>
-        <div className="item18">0</div>
-        <div className="item">.</div>
-        <div className="item2">=</div>
+
+        {btns.map(({ value, className }) => (
+          <button
+            type="button"
+            className={className}
+            value={value}
+            key={value}
+            onClick={this.onClickHandler}
+          >
+            {value}
+          </button>
+        ))}
       </div>
     );
   }
