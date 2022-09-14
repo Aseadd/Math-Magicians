b09045ea070d4465969487783938fec52d8081bd
addis <betsega23@gmail.com>
2022-09-14 21:02:25 +0300

    Add math quote functionality

---
+++ src/components/quotes.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Quotes = () => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const response = await fetch(
+        'https://random-math-quote-api.herokuapp.com/',
+      );
+      const data = await response.json();
+      // const parseData = JSON.parse(data);
+      setQuote(data);
+      console.log('data', data);
+    };
+    fetchQuotes();
+  }, []);
+  console.log(quote.quote);
+  return (
+    <div className="quotes">
+      <p className="quote">
+        {quote.quote}
+        &nbsp;
+        <strong>{quote.author}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Quotes;
