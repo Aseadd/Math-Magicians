d1132b7ca4b31796c2ebbb708fbc7e9345079ec1
addis <betsega23@gmail.com>
2022-09-15 15:20:57 +0300

    Add Additional tests for calculate and operate functions

--- src/components/__test__/calculate.test.js
+++ src/components/__test__/calculate.test.js
@@ -5,3 +5,35 @@ it('should return 15 when 7+8=', () => {
   const result = btns.reduce((acc, btn) => calculate(acc, btn.value), {});
   expect(result.total).toBe('15');
 });
+
+const btns2 = [
+  { value: '7' },
+  { value: '+' },
+  { value: '8' },
+  { value: '=' },
+  { value: 'x' },
+  { value: '2' },
+  { value: '=' },
+];
+it('should return 30 when 7+8= x 2=', () => {
+  const result = btns2.reduce((acc, btn) => calculate(acc, btn.value), {});
+  expect(result.total).toBe('30');
+});
+
+const btns3 = [
+  { value: '7' },
+  { value: '+' },
+  { value: '8' },
+  { value: '=' },
+  { value: 'AC' },
+];
+it('should return 0 when 7+8= AC', () => {
+  const result = btns3.reduce((acc, btn) => calculate(acc, btn.value), {});
+  expect(result.total).toBe(null);
+});
+
+const btns4 = [{ value: '7' }, { value: '+/-' }];
+it('should return -7 when 7+/-', () => {
+  const result = btns4.reduce((acc, btn) => calculate(acc, btn.value), {});
+  expect(result.next).toBe('-7');
+});
--- src/components/__test__/operate.test.js
+++ src/components/__test__/operate.test.js
@@ -4,3 +4,9 @@ it('should return 15 when 7+8=', () => {
   const result = operate('7', '8', '+');
   expect(result).toBe('15');
 });
+
+const numbers = ['7', '8', '4'];
+it('should return 60 when 7+8= x 2=', () => {
+  const result = numbers.reduce((acc, num) => operate(acc, num, '-'), '20');
+  expect(result).toBe('1');
+});
