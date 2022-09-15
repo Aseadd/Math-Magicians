093bfea29c8a7eb9e3381ba0a8011a2d916d204d
addis <betsega23@gmail.com>
2022-09-15 16:02:21 +0300

    Add user interaction test for calculator button

--- src/components/__test__/calculator.test.js
+++ src/components/__test__/calculator.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from '../calculator';
 
 it('should render the calculator component', () => {
@@ -11,4 +12,18 @@ it('should render the calculator component', () => {
 it('should match the snapshot', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('Renders user event correctly and display 1', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.getByRole('button', { name: '1' }));
+  const result = screen.getByRole('button', { name: '1' });
+  expect(result.innerHTML).toBe('1');
+});
+
+it('Renders user event correctly and display AC', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.getByRole('button', { name: 'AC' }));
+  const result = screen.getByRole('button', { name: 'AC' });
+  expect(result.innerHTML).toBe('AC');
 });
